19bf275e0efe2b35b99381b8981f87996fdfa246
Yu Jing <yujing5b5d@gmail.com>
2017-06-18 16:05:12 -0400

    update start script, print logs

--- scripts/zeppelin_serve.sh
+++ scripts/zeppelin_serve.sh
@@ -3,3 +3,6 @@ source /etc/profile
 source /etc/bashrc
 
 /usr/bin/sudo -i -u zeppelin /usr/local/zeppelin/bin/zeppelin-daemon.sh start
+
+sleep 3 # wait for the files creating...
+tail -f /usr/local/zeppelin/logs/*
