8a9c000fbbd909718ca154abcf5f67501dcc369d
Yu Jing <yujing5b5d@gmail.com>
2017-09-09 23:08:11 -0400

    update config

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,7 +6,7 @@ export INSTALL_ZEPPELIN=1
 
 # the follow 3 options are based on miniconda
 export INSTALL_THEANO=1
-export INSTALL_PYTORCH=1
+export INSTALL_PYTORCH=0
 export INSTALL_TENSORFLOW=1
 
 
